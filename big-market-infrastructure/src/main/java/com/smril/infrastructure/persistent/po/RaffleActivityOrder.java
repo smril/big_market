5db0109b0b35448cb76ac3d96a0ab169c6dd8f13
smril <smrilmax@outlook.com>
2024-08-03 16:57:34 +0800

    feat: 第12节完成

---
+++ big-market-infrastructure/src/main/java/com/smril/infrastructure/persistent/po/RaffleActivityOrder.java
@@ -0,0 +1,65 @@
+package com.smril.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author smril
+ * @create 2024/8/3 16:49
+ * @description 抽奖活动单 持久化对象
+ */
+
+@Data
+public class RaffleActivityOrder {
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+
+    /**
+     * 活动名称
+     */
+    private String activityName;
+
+    /**
+     * 抽奖策略ID
+     */
+    private Long strategyId;
+
+    /**
+     * 订单ID
+     */
+    private String orderId;
+
+    /**
+     * 下单时间
+     */
+    private Date orderTime;
+
+    /**
+     * 订单状态（not_used、used、expire）
+     */
+    private String state;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
